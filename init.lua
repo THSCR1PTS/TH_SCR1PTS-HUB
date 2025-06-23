12dfdcf7d420f2f60be712b859337ee3efee61f0
THSCR1PTS <happynationanonimo@gmail.com>
2025-06-23 01:04:44 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,27 @@
+-- TH_SCR1PTS HUB - Script para Brookhaven usando Rayfield GUI
+-- Link raw para loadstring: https://raw.githubusercontent.com/SEUUSUARIO/TH_SCR1PTS-HUB/main/init.lua
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "TH_SCR1PTS HUB",
+    LoadingTitle = "TH_SCR1PTS HUB",
+    LoadingSubtitle = "by TH_SCR1PTS",
+    ConfigurationSaving = { Enabled = false }
+})
+
+-- Aba Player
+local PlayerTab = Window:CreateTab("Player")
+
+PlayerTab:CreateSlider({
+    Name = "WalkSpeed",
+    Range = {16, 500},
+    Increment = 1,
+    CurrentValue = 16,
+    Callback = function(value)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+    end
+})
+
+-- Continue adicionando suas funções aqui
 
